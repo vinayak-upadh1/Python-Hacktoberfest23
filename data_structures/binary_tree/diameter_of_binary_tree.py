f3e451d7df0a62ce2fc0b0670d6536061d8d4de8
Vinayak Upadhyay <vinayakupadhyay42@gmail.com>
2023-10-16 14:54:25 +0530

    Added and modified diameter of tree file

--- data_structures/binary_tree/diameter_of_binary_tree.py
+++ data_structures/binary_tree/diameter_of_binary_tree.py
@@ -4,70 +4,133 @@ The diameter/width of a tree is defined as the number of nodes on the longest pa
 
 # A binary tree Node
 
+import sys
+from collections import deque
+
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-  
+
 # utility class to pass height object
+
+
 class Height:
-	def __init__(self):
-		self.h = 0
+    def __init__(self):
+        self.h = 0
 
 
 # Function to calculate diameter of the given binary tree
-def calculate_diameter(root:Node, height:int) -> int:
+def calculate_diameter(root: Node, height: int) -> int:
 
-	# to store height of left and right subtree
-	lh = Height()
-	rh = Height()
+    # to store height of left and right subtree
+    lh = Height()
+    rh = Height()
 
-	# base condition- when binary tree is empty
-	if root is None:
-		height.h = 0
-		return 0
+    # base condition- when binary tree is empty
+    if root is None:
+        height.h = 0
+        return 0
 
-	# ldiameter --> diameter of left subtree
-	# rdiameter --> diameter of right subtree
+    # ldiameter --> diameter of left subtree
+    # rdiameter --> diameter of right subtree
 
-	# height of left subtree and right subtree is obtained from lh and rh and returned value of function is stored in ldiameter and rdiameter
+    # height of left subtree and right subtree is obtained from lh and rh and returned
+    # value of function is stored in ldiameter and rdiameter
 
-	ldiameter = calculate_diameter(root.left, lh)
-	rdiameter = calculate_diameter(root.right, rh)
+    ldiameter = calculate_diameter(root.left, lh)
+    rdiameter = calculate_diameter(root.right, rh)
 
-	# height of tree will be max of left subtree
-	# height and right subtree height plus1
+    # height of tree will be max of left subtree
+    # height and right subtree height plus1
 
-	height.h = max(lh.h, rh.h) + 1
+    height.h = max(lh.h, rh.h) + 1
 
-	# return maximum of the following
-	# 1)left diameter
-	# 2)right diameter
-	# 3)left height + right height + 1
-	return max(lh.h + rh.h + 1, max(ldiameter, rdiameter))
+    # return maximum of the following
+    # 1)left diameter
+    # 2)right diameter
+    # 3)left height + right height + 1
+    return max(lh.h + rh.h + 1, max(ldiameter, rdiameter))
 
 
-def diameter(root:Node) -> int:
-	height = Height()
-	return calculate_diameter(root, height)
+def diameter(root: Node) -> int:
+    height = Height()
+    return calculate_diameter(root, height)
+
+
+sys.setrecursionlimit(50000)
+# Tree Node
+
+
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    # Corner Case
+    if(len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size+1
+
+    # Starting from the second element
+    i = 1
+    while(size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size-1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if(currVal != "N"):
+
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size+1
+        # For the right child
+        i = i+1
+        if(i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if(currVal != "N"):
+
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size+1
+        i = i+1
+    return root
 
 
 if __name__ == "__main__":
-	root = Node(1)
-	root.left = Node(2)
-	root.right = Node(3)
-	root.left.left = Node(4)
-	root.left.right = Node(5)
-
-	"""
-Constructed binary tree is 
-		1
-	   / \
-	  2	  3
-	 / \
-	4	 5
-"""
-
-	print("The diameter of the binary tree is:", end=" ")
-	print(diameter(root))
+    s = input()
+    root = buildTree(s)
+    calculated_diameter = diameter(root)
+    print(calculated_diameter)
